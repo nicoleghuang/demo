b1c43cd89354008b42f66606fc81fa77c2034753
nicoleghuang <1107910050@qq.com>
2017-10-23 17:02:37 +0800

    module Tkinter

---
+++ learnTkinter/nameHello.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import messagebox as messagebox
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput=Entry(self)
+        self.nameInput.pack()
+        self.alertButton=Button(self,text="Hello",command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name=self.nameInput.get() or "World"
+        messagebox.showinfo("Message","Hello,%s" %name)
+
+
+app=Application()
+app.master.title("hello")
+app.mainloop()
+
+
+
+
