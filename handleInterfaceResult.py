959857f2e6b7f6feb912f21d99f32925d3dcebc8
nicoleghuang <1107910050@qq.com>
2019-10-13 18:12:42 +0800

    Update handleInterfaceResult.py

--- handleInterfaceResult.py
+++ handleInterfaceResult.py
@@ -1,17 +1,11 @@
 import requests
-import re
 import json
-#r1=requests.get("http://piaofang.maoyan.com/second-box")
-#print(r1.text)
-
 def require():
-    import requests
-    import json
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'}
+    
     url = 'http://piaofang.maoyan.com/second-box'
-    #s = json.dumps({'coin_type': 'bch', 'coin_address': '192Vrf3A9HZU3BhSYatm4eTeVAadASZpxH','actual_amount':'0.001','access_id':'C16F3EF3EB3A4391B1C1FA64CBBDD70A','tonce':y})
     r= requests.get(url, headers=headers)
     return r.text
 r1=require()
